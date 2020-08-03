575472cd002bb815bc7fefd48dc548d4bcc52f2a
ramesh <ramesh.sharma@roambee.com>
2020-08-03 23:30:41 +0530

    SSR integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     NgbModule,
     CoreModule,
